a6ba4ce5a6df5abb1db77326234776bcf2c0db2c
Nicolas Keklikian <nicolas.keklikian@rappi.com>
2024-03-06 22:28:26 -0300

    Mock Git response in job manager test

--- tests/test_job_manager.py
+++ tests/test_job_manager.py
@@ -1,9 +1,11 @@
-from app.home.ip_blocklist_service import IpBlocklistService
+from app.home.ip_blocklist_service import IpBlocklistService, s
 from app.home.constants import BLOCKLIST_NAME, LAST_COMMIT_SHA_KEY
+from app.home.config import URL
 from app.redis_service import redis_instance
 from app.home.job_manager import (save_ip_blocklist, download_and_save_blocklist,
                                   update_last_commit, get_current_commit_sha, update_blocklist_if_needed)
 from tests.redis_instance_test import redis_test_instance
+import requests_mock
 
 
 def test_save_ip_blocklist():
@@ -13,7 +15,15 @@ def test_save_ip_blocklist():
 
 def test_download_and_save_blocklist():
     download_and_save_blocklist()
-    assert redis_test_instance.sismember(BLOCKLIST_NAME, '213.109.202.127') # Mock github response
+    assert redis_test_instance.sismember(BLOCKLIST_NAME, '213.109.202.127')
+
+
+def test_mocked_download_and_save_blocklist():
+    adapter = requests_mock.Adapter()
+    s.mount(URL, adapter)
+    adapter.register_uri('GET', URL, text='1.2.3.4')
+    download_and_save_blocklist()
+    assert redis_test_instance.sismember(BLOCKLIST_NAME, '1.2.3.4')
 
 
 def test_update_last_commit():
